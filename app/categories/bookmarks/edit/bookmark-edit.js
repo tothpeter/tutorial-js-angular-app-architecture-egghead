3d27061be3eb577cbd77322a0a80aa8ca7927e79
Peter Toth <tothpeter08@gmail.com>
2016-08-25 16:06:18 +0200

    Add updating bookmark functionality

--- app/categories/bookmarks/edit/bookmark-edit.js
+++ app/categories/bookmarks/edit/bookmark-edit.js
@@ -7,6 +7,36 @@ angular.module('categories.bookmarks.edit', [])
         controller: 'EditBookmarkCtrl as editBookmarkCtrl'
       });
   })
-  .controller('EditBookmarkCtrl', function() {
+  .controller('EditBookmarkCtrl', function($state, $stateParams, BookmarksModel) {
+    var editBookmarkCtrl = this;
 
+    function returnToBookmarsk() {
+      $state.go('eggly.categories.bookmarks', {
+        category: $stateParams.category
+      });
+    }
+
+    function cancelEditing() {
+      returnToBookmarsk();
+    }
+
+    function setEditedBookmark() {
+      BookmarksModel.findById($stateParams.bookmarkId).then(function(bookmark) {
+        if (bookmark) {
+          editBookmarkCtrl.editedBookmark = angular.copy(bookmark);
+        } else {
+          returnToBookmarsk();
+        }
+      });
+    }
+
+    function updateBookmark(bookmark) {
+      BookmarksModel.updateBookmark(bookmark);
+    }
+
+
+    editBookmarkCtrl.updateBookmark = updateBookmark;
+    editBookmarkCtrl.cancelEditing = cancelEditing;
+
+    setEditedBookmark();
   });
--- app/common/models/bookmarks-model.js
+++ app/common/models/bookmarks-model.js
@@ -1,5 +1,5 @@
 angular.module('eggly.models.bookmarks', [])
-  .service('BookmarksModel', function($http) {
+  .service('BookmarksModel', function($http, $q) {
     var model = this,
         urls = {
           fetch: 'data/bookmarks.json'
@@ -15,12 +15,51 @@ angular.module('eggly.models.bookmarks', [])
       return bookmarks;
     }
 
+    function find(id) {
+      return _.find(bookmarks, function(bookmark) {
+        return bookmark.id === parseInt(id, 10);
+      });
+    }
+
+    model.findById = function(id) {
+      var deferred = $q.defer();
+
+      if (bookmarks) {
+        deferred.resolve(find(id))
+      } else {
+        model.getBookmarks().then(function() {
+          deferred.resolve(find(id))
+        });
+      }
+
+      return deferred.promise;
+    }
+
     model.getBookmarks = function() {
-      return $http.get(urls.fetch).then(cacheData);
+      var deferred = $q.defer();
+
+      if (bookmarks) {
+        deferred.resolve(bookmarks)
+      } else {
+        $http.get(urls.fetch).then(function(result) {
+          deferred.resolve(cacheData(result))
+        });
+      }
+
+      return deferred.promise;
     };
 
-    model.createBookmark= function(bookmark) {
+    model.createBookmark = function(bookmark) {
       bookmark.id = bookmarks.length;
       bookmarks.push(bookmark);
+    };
+
+    model.updateBookmark = function(bookmarkToUpdate) {
+      bookmarkIndex = _.findIndex(bookmarks, function(bookmark) {
+        return bookmarkToUpdate.id === bookmark.id;
+      });
+
+      bookmarks[bookmarkIndex] = bookmarkToUpdate;
+      return bookmarks[bookmarkIndex];
     };
   });
